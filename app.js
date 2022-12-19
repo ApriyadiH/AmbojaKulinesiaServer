598aed3dd484d89c513e300aab3c954474450667
Apriyadi Hegarto <ahegarto@gmail.com>
2022-12-19 14:21:07 +0700

    tambah folder

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// API untuk mengetes server
+app.get("/", (req, res) => {
+  res.send("Testing ayam goreng");
+});
+
+app.listen(port, () => {
+  console.log(port, "Server is open with port!");
+});
