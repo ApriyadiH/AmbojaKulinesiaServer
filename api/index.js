f88bda8688677773e26a78834eb19871ae5388a0
Apriyadi Hegarto <ahegarto@gmail.com>
2022-12-21 20:52:39 +0700

    tambah tester

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ const loginRouter = require("./login");
 const foodsRouter = require("./foodPosts");
 const userRouter = require("./user");
 const adminRouter = require("./admin");
+const testRouter = require("./test");
 
 module.exports = [
     signupRouter,
     loginRouter,
     foodsRouter,
     userRouter,
-    adminRouter
+    adminRouter,
+    testRouter
 ];
